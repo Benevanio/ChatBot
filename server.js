b9d9d38a02bc2a1ce511afa1b3a8f14d77682cb0
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-02 20:24:18 -0300

    Socket IO conectd server on

--- server.js
+++ server.js
@@ -3,10 +3,21 @@ const app = express();
 const port = 3002;
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-
+app.use(express.static(__dirname + '/public'));
 http.listen(port, () => {
     console.log(`Listening on port ${port}`);
     }
 );
-
-
+app.get('/', (req,res,next) => {
+    res.sendFile(__dirname + '/index.html');
+});
+io.on('connection', (socket) => {
+    console.log('User connected');
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
